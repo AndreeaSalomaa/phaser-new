fa0b57efabdac05e795557701b52b8d9c55bff77
Vedran Brnjetic <vedran.brnjetic@metropolia.fi>
2018-05-16 20:54:59 +0300

    Added floor, cactii and score text

--- src/scenes/gamescene.js
+++ src/scenes/gamescene.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import background from "../assets/Background.jpg";
-import floors from "../assets/floor.png";
+import floor from "../assets/floor.png";
+import cactus from "../assets/Cactus.png";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -9,17 +10,22 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload(){
-    this.load.image("floor", floors);
     this.load.image("background", background);
+    this.load.image("floor", floor);
+    this.load.image("cactus", cactus);
   }
 
   create () {
-    this.add.image(533,300, 'background').setScale(0.83333).refreshBody();
+    this.add.image(533,300, 'background').setScale(0.83333);
     var platforms = this.physics.add.staticGroup();
 
-    platforms.create(533, 600-24, "floor").setScale(0.83333).refreshBody();
+    platforms.create(533, 600-24, "floor").setScale(0.83333);
 
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    var cactii = this.physics.add.staticGroup();
+    cactii.create(533, 600-50, "cactus").setScale(0.4);
+    cactii.create(310, 600-50, "cactus").setScale(0.4);
+    cactii.create(800, 600-50, "cactus").setScale(0.4).refreshBody();
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#FFF' });
 
   }
   update(deltaTime) {
