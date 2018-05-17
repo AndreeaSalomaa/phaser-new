e939a9a46f863a1c72389488d208668e6a1eb58a
Vedran Brnjetic <vedran.brnjetic@metropolia.fi>
2018-05-17 07:23:04 +0300

    Playing with clouds

--- src/scenes/gamescene.js
+++ src/scenes/gamescene.js
@@ -19,35 +19,58 @@ export default class GameScene extends Phaser.Scene {
 
   create () {
     this.add.image(533,300, 'background').setScale(0.83333);
-    this.add.image(533,64, 'cloud').setScale(0.4);
-    this.add.image(239,124, 'cloud').setScale(0.4);
-    this.add.image(728,94, 'cloud').setScale(0.2);
-    this.add.image(58,84, 'cloud').setScale(0.2);
-    this.add.image(798,114, 'cloud').setScale(0.3);
-    var platforms = this.physics.add.staticGroup();
 
+    //some cloud
+    this.clouds = [];
+    this.clouds.push(this.physics.add.sprite(533,64, 'cloud').setScale(0.4));
+    this.clouds.push(this.physics.add.sprite(239,124, 'cloud').setScale(0.4));
+    this.clouds.push(this.physics.add.sprite(728,94, 'cloud').setScale(0.2));
+    this.clouds.push(this.physics.add.sprite(58,84, 'cloud').setScale(0.2));
+    this.clouds.push(this.physics.add.sprite(798,114, 'cloud').setScale(0.3));
+    //kill gravity on clouds
+    this.clouds.map((cloud)=>cloud.body.gravity.y= -200);
+
+
+    //floor for our hero to walk on.
+    var platforms = this.physics.add.staticGroup();
     platforms.create(533, 600-24, "floor").setScale(0.83333);
 
     //Smaller cactii in the background that don't affect the player
     var bg_cactii = this.physics.add.staticGroup();
-
     bg_cactii.create(513, 600-150, "cactus").setScale(0.3);
     bg_cactii.create(513, 300, "cactus").setScale(0.15);
     bg_cactii.create(623, 320, "cactus").setScale(0.15);
 
     //"enemy" cactii that collide with player
     var cactii = this.physics.add.staticGroup();
-
-    cactii.create(533, 600-50, "cactus").setScale(0.4);
-    cactii.create(310, 600-50, "cactus").setScale(0.4);
+    cactii.create(533, 600-55, "cactus").setScale(0.4);
+    cactii.create(310, 600-45, "cactus").setScale(0.4);
     cactii.create(800, 600-50, "cactus").setScale(0.4).refreshBody();
+
+
     var scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#FFF' });
     var balloonsText = this.add.text(848, 16, 'Balloons: 3', { fontSize: '32px', fill: '#FFF' });
 
 
+    this.cursors = this.input.keyboard.createCursorKeys();
+
 
   }
   update(deltaTime) {
-
+      if (this.cursors.right.isDown)
+      {
+          this.clouds.map((cloud)=>cloud.setVelocityX(Math.floor(Math.random() * 3)+5));
+          //player.anims.play('left', true);
+      }
+      if (this.cursors.left.isDown)
+      {
+          this.clouds.map((cloud)=>cloud.setVelocityX(-(Math.floor(Math.random() * 3)+5)));
+          //player.anims.play('left', true);
+      }
+      if (this.cursors.down.isDown)
+      {
+          this.clouds.map((cloud)=>cloud.setVelocityX(0));
+          //player.anims.play('left', true);
+      }
   }
 }
